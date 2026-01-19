7fb01290e7e4d7e7ddcf35f47ba273e9824d7def
laryssarodrigues <laryssa.rodrigues@ufrpe.br>
2026-01-19 17:35:08 -0300

    add examples of all hero types

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Hero.title();
 
-        Hero hero1 = new Hero();
-        hero1.name = "Anny";
-        hero1.age = 230;
-        hero1.type = "wizard";
-        hero1.attack();
+        Hero[] heroes = {
+                new Hero("Holly", 23, "wizard"),
+                new Hero("Way", 34, "warrior"),
+                new Hero("Poo", 47, "monk"),
+                new Hero("Lynn", 52, "ninja"),
+                new Hero("Klow", 30, "archer")
+        };
 
-        Hero hero2 = new Hero();
-        hero2.name = "Blomm";
-        hero2.age = 198;
-        hero2.type = "ninja";
-        hero2.attack();
+        for (int i = 0; i < heroes.length; i++) {
+            System.out.println("-> Hero " + (i + 1) + ": " + heroes[i].name);
+            System.out.print("Result: ");
+            heroes[i].attack();
+        }
     }
 }
